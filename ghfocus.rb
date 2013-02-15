01f0e2a367c1c1c9db00dc4b05445e4d90ac4125
Daniel Schauenberg <d@unwiredcouch.com>
2013-02-15 00:20:44 -0500

    add ghfocus script

---
+++ ghfocus.rb
@@ -0,0 +1,112 @@
+#!/usr/bin/env ruby
+
+#
+# create an oauth token with:
+# curl -k -s -u mrtazz -d '{ "scopes": [ "repo" ], "note": "ghfocus cli"}'\
+# -X POST https://api.github.com/authorizations
+#
+# Then create a ~/.ghfocus file with the following content format:
+# -
+#   GHHOST: "https://api.github.com"
+#   GHPATH: "/issues"
+#   TOKEN: "token"
+#   CONTEXT: "GitHub"
+#   GHNAME: "GitHub"
+#
+
+$VERBOSE = nil
+
+require "rubygems"
+require "appscript"
+require "yaml"
+require "pp"
+require "faraday"
+require "json"
+
+CONFIG = YAML.load_file(File.join(ENV["HOME"], ".ghfocus"))
+CONFIG.each do |c|
+  GHHOST  = c["GHHOST"]
+  GHPATH  = c["GHPATH"]
+  TOKEN   = c["TOKEN"]
+  CONTEXT = c["CONTEXT"]
+  GHNAME  = c["GHNAME"]
+
+  # Public: get all issues for a user corresponding to a given OAuth token
+  # across owned, member and organization repositories
+  #
+  # Params:
+  #   token - OAuth token for a user
+  #   host  - github host
+  #   path  - path to issues API
+  #
+  # Returns an array of GitHub issues of the format [{:title, :repo, :url}]
+  def get_github_issues(token, host="https://api.github.com", path="/issues")
+    conn = Faraday.new(:url => host)
+    res = conn.get do |req|
+      req.url path
+      req.headers['Authorization'] = "token #{token}"
+    end
+    res = JSON.parse(res.body)
+    res.map! {|item| {:summary => item["title"],
+                      :repo => item["repository"]["full_name"],
+                      :url => item["html_url"],
+                      :reporter => item["user"]["login"]
+                      }}
+  end
+
+
+  # hash to hold all data from omnifocus
+  omni_data = Hash.new()
+  gh_data = Hash.new()
+
+  issues = get_github_issues(TOKEN, GHHOST, GHPATH)
+  issues.each do |issue|
+    id = "#{issue[:repo]}/#{issue[:url].split("/")[-1]}"
+    gh_data[id] = issue
+  end
+
+  # connect to OmniFocus
+  omnifocus = Appscript.app('OmniFocus')
+  omnifocus_jira = omnifocus.default_document
+  jira_context = omnifocus.default_document.flattened_contexts[CONTEXT]
+
+  omnifocus_jira.flattened_tasks.get.each do |task|
+      name = task.name.get
+      created = task.creation_date.get
+
+      if name =~ /^([[:alpha:]]+\/[[a-zA-Z0-9]]+\/\d+) - (.*?) \(([^\)]+)\)$/
+          key = $1
+          summary = $2
+          reporter = $3
+
+          omni_data[key] = Hash.new()
+          omni_data[key][:summary]  = summary
+          omni_data[key][:reporter] = reporter
+          omni_data[key][:created]  = created
+      end
+  end
+
+  ## TODO: need to handle jobs being closed in jira (are completed in OF), and completed in OF - use comment in notes field of OF as the closing DONE comment in jira
+
+  # sync jira to OF
+  skipped = 0
+  synced  = 0
+  gh_data.each do |key,props|
+      if omni_data.has_key?(key)
+          skipped += 1
+          ## TODO: update name and reporter if it has changed
+      else
+          # TODO: fix timezones (inserting as UTC)
+          name = sprintf("%s - %s (%s)", key, props[:summary], props[:reporter])
+          link = props[:url]
+          desc = sprintf("REF: %s\n\n%s", link, props[:desc])
+
+          puts "** Adding #{key}"
+          omnifocus_jira.make(:new => :inbox_task, :with_properties => { :name => name, :creation_date => props[:created], :note => desc, :context => jira_context })
+          synced += 1
+      end
+  end
+
+  puts "** Synced #{synced} issues from #{GHNAME} to OmniFocus, Skipped #{skipped}"
+
+end
