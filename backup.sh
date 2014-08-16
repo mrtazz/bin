3b9afb87735d2cd477f6a559d7953b50e054f3e3
Daniel Schauenberg <d@unwiredcouch.com>
2014-08-16 15:49:47 +0000

    add verbose flag to backup script

--- backup.sh
+++ backup.sh
@@ -2,9 +2,13 @@
 #
 # Backup script to pull in changes from remote hosts
 #
+if [ "z$1" = "z--verbose" ]; then
+  ARG="-v"
+fi
+
 for backup in $(ls /backup); do
   grep -q ${backup} ~/.backupexcludes
   if [ $? != 0 ]; then
-    /usr/local/bin/rsync -e 'ssh -o BatchMode=yes -o ConnectTimeout=10' --archive --delete --timeout=5 ${backup}:. /backup/${backup}/
+    /usr/local/bin/rsync ${ARG} -e 'ssh -o BatchMode=yes -o ConnectTimeout=20' --archive --delete --timeout=20 ${backup}:. /backup/${backup}/
   fi
 done
