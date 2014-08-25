1e5d2d35d49de55b7ab5cf31fef4e5d8f54c2865
Daniel Schauenberg <d@unwiredcouch.com>
2014-08-25 10:52:16 +0000

    show hostname in backup script in verbose mode

--- backup.sh
+++ backup.sh
@@ -3,12 +3,14 @@
 # Backup script to pull in changes from remote hosts
 #
 if [ "z$1" = "z--verbose" ]; then
+  VERBOSE=1
   ARG="-v"
 fi
 
 for backup in $(ls /backup); do
   grep -q ${backup} ~/.backupexcludes
   if [ $? != 0 ]; then
+    [ ! -z "${VERBOSE}" ] && echo "Backing up ${backup}..."
     /usr/local/bin/rsync ${ARG} -e 'ssh -o BatchMode=yes -o ConnectTimeout=20' --archive --delete --timeout=20 ${backup}:. /backup/${backup}/
   fi
 done
