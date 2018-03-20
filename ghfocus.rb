8309e5d921ec760d48c087eb83124f9bdfb3eee7
Daniel Schauenberg <d@etsy.com>
2018-03-20 16:10:03 +0100

    switch ghfocus to use search API

--- ghfocus.rb
+++ ghfocus.rb
@@ -1,10 +1,8 @@
 #!/usr/bin/env ruby
 
 #
-# Script to fetch all issues assigned to you from GitHub (Enterprise) and add
-# it to your OmniFocus inbox with the configured context
-#
-# Thanks to @ickymettle for inspiration and letting me base this on his code
+# Script to fetch all issues assigned to you and reviews requested from GitHub
+# (Enterprise) and add it to your OmniFocus inbox with the configured context
 #
 # create an oauth token with:
 # curl -k -s -u mrtazz -d '{ "scopes": [ "repo" ], "note": "ghfocus cli"}'\
@@ -17,6 +15,7 @@
 #   TOKEN: "token"
 #   CONTEXT: "GitHub"
 #   GHNAME: "GitHub"
+#   GHSEARCHTERM: "mrtazz+is:open+is:pr"
 #
 
 $VERBOSE = nil
@@ -28,6 +27,16 @@ require "pp"
 require "faraday"
 require "json"
 
+class DoNotEncoder
+  def self.encode(params)
+    buffer = ''
+    params.each do |key, value|
+      buffer << "#{key}=#{value}&"
+    end
+    return buffer.chop
+  end
+end
+
 CONFIG = YAML.load_file(File.join(ENV["HOME"], ".ghfocus"))
 CONFIG.each do |c|
   GHHOST  = c["GHHOST"]
@@ -35,9 +44,13 @@ CONFIG.each do |c|
   TOKEN   = c["TOKEN"]
   CONTEXT = c["CONTEXT"]
   GHNAME  = c["GHNAME"]
+  GHSEARCHTERM  = c["GHSEARCHTERM"]
 
   # Public: get all issues for a user corresponding to a given OAuth token
   # across owned, member and organization repositories
+  #
+  # This is based on the following search:
+  # https://api.github.com/search/issues?q=type:pr+review-requested:mrtazz
   #
   # Params:
   #   token - OAuth token for a user
@@ -45,18 +58,25 @@ CONFIG.each do |c|
   #   path  - path to issues API
   #
   # Returns an array of GitHub issues of the format [{:title, :repo, :url}]
-  def get_github_issues(token, host="https://api.github.com", path="/issues")
-    conn = Faraday.new(:url => host)
+  def get_github_issues(token, host="https://api.github.com", path="/search/issues")
+    issues = []
+    conn = Faraday.new(:url => host, request: { params_encoder: DoNotEncoder })
     res = conn.get do |req|
-      req.url path
+      req.url path, :q => GHSEARCHTERM
       req.headers['Authorization'] = "token #{token}"
     end
     res = JSON.parse(res.body)
-    res.map! {|item| {:summary => item["title"],
-                      :repo => item["repository"]["full_name"],
-                      :url => item["html_url"],
-                      :reporter => item["user"]["login"]
-                      }}
+    res["items"].each do |item|
+      repo = item["repository_url"].split("/").last(2).join("/")
+      issues << {
+        :summary => item["title"],
+        :repo => repo,
+        :url => item["html_url"],
+        :reporter => item["user"]["login"]
+      }
+    end
+
+    return issues
   end
 
 
